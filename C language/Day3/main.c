af2e92fa853db50f05f9b3f31b7c9e2737535a78
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-18 10:25:22 +0530

    Add files via upload

--- C language/Day3/main.c
+++ C language/Day3/main.c
@@ -95,3 +95,12 @@
 
 
 //=========================================
+// WAP to print number 1 to 10
+#include<stdio.h>
+int main(){
+  int i;
+  for(i = 1; i <= 10; i++){
+    printf("%d\n",i);
+  }
+  return 0;
+}
