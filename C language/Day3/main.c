2cc006360c55f3baf3212392d9c20a57b651da23
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-18 09:31:59 +0530

    Add files via upload

--- C language/Day3/main.c
+++ C language/Day3/main.c
@@ -16,17 +16,32 @@
 
 //===========================================
 // WAP  to check wheather the character entered by the user is VOWEL or CONSONENT...
+// #include<stdio.h>
+// int main(){
+//   char ch;
+//   printf("Enter a character: ");
+//   scanf("%c", &ch);
+  
+//   if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+//     printf("%c is a vowel. \n", ch);
+//   } else {
+//     printf("%c is consonent. \n", ch);
+//   }
+
+//   return 0;
+// }
+
+
+//========================================
+// find area of circle. formul is a = PI * r*r
 #include<stdio.h>
 int main(){
-  char ch;
-  printf("Enter a character: ");
-  scanf("%c", &ch);
-  
-  if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
-    printf("%c is a vowel. \n", ch);
-  } else {
-    printf("%c is consonent. \n", ch);
-  }
+  float radius, area;
+  printf("Enter the radius of circle:");
+  scanf("%f", &radius);
 
+  area = 3.14 * radius * radius;
+  
+  printf("Area of circle is: %.2f", area);
   return 0;
 }
