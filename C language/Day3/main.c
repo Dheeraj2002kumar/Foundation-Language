4e3a5f15e9ebc57ab3e313da54f2b4d1f0670d75
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-18 11:09:24 +0530

    Add files via upload

--- C language/Day3/main.c
+++ C language/Day3/main.c
@@ -165,3 +165,16 @@
 // }
 
 
+//=========================================
+// WAP to find out the sum of N natural numbers.
+#include<stdio.h>
+int main(){
+  int n, i, sum = 0;
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  for(i = 1; i <= n; i++){
+    sum = sum + i;
+  }
+  printf("Sum of %d natural numbers is: %d", n, sum);
+  return 0;
+}
