c98629348edd146f4715cb954685f06e9258f5d8
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-18 09:35:39 +0530

    Add files via upload

--- C language/Day3/main.c
+++ C language/Day3/main.c
@@ -34,13 +34,27 @@
 
 //========================================
 // find area of circle. formul is a = PI * r*r
+// #include<stdio.h>
+// int main(){
+//   float radius, area;
+//   printf("Enter the radius of circle:");
+//   scanf("%f", &radius);
+
+//   area = 3.14 * radius * radius;
+  
+//   printf("Area of circle is: %.2f", area);
+//   return 0;
+// }
+
+// another way use #define PI 3.14
 #include<stdio.h>
+#define PI 3.14
 int main(){
   float radius, area;
   printf("Enter the radius of circle:");
   scanf("%f", &radius);
 
-  area = 3.14 * radius * radius;
+  area = PI * radius * radius;
   
   printf("Area of circle is: %.2f", area);
   return 0;
