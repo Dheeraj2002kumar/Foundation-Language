4cac86ebe133cc0fd5f03fdd7516a28c9e9eeede
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-19 09:20:30 +0530

    Add files via upload

--- C language/Day4/main.c
+++ C language/Day4/main.c
@@ -32,18 +32,47 @@
 //=====================================================
 
 // Program to add numbers until the user enters zero
+// #include <stdio.h>
+
+// int main(){
+//   double number, sum = 0;
+
+//   // the body of the loop is executed at least once 
+//   do {
+//     printf("Enter a number: ");
+//     scanf("%lf", &number);
+//     sum += number;
+//   }
+//   while (number != 0.0);
+//   printf("sum = %.2lf", sum);
+//   return 0;
+// }
+
+
+
+//==============================================
+
+// Program to calculate the sum of numbers (10 numbers max) // if the user enters a negative number, the loop terminates
+
 #include <stdio.h>
 
 int main(){
-  double number, sum = 0;
 
-  // the body of the loop is executed at least once 
-  do {
-    printf("Enter a number: ");
+  int i;
+  double number, sum = 0.0;
+
+  for(i = 1; i <= 10; ++i){
+    printf("Enter number %d: ", i);
     scanf("%lf", &number);
+
+    // if the user enters a negative number, break the loop
+    if(number < 0.0){
+      break;
+    }
     sum += number;
+    // sum = sum + number
   }
-  while (number != 0.0);
-  printf("sum = %.2lf", sum);
+
+  printf("Sum = %.2f", sum);
   return 0;
 }
