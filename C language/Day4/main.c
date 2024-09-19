75714f48b1ba45409ba7f27250d65a97a3791761
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-19 15:59:14 +0530

    Add files via upload

--- C language/Day4/main.c
+++ C language/Day4/main.c
@@ -148,25 +148,50 @@
 //==================================================================
 // Program to calculate the sum and average of positive number
 // If the user enters a negative number, the  sum and average are displayed.
+// #include <stdio.h>
+// int main(){
+
+//   const int maxInput = 100;
+//   int i;
+//   double number, average, sum = 0.0;
+
+//   for(i = 1; i <= maxInput; i++){
+//     printf("%d Enter a number ", i);
+//     scanf("%lf", &number);
+
+//     // go to jump if the user enters a negative number
+//     if(number < 0.0){
+//       goto jump;
+//     }
+//     sum += number;
+//   }
+//   jump:average = sum / (i - 1);
+//   printf("Sum = %.2f\n", sum);
+//   printf("Average = %.2f", average);
+//   return 0;
+// }
+
+
+//=========================================================
+
+// Check whether number is positive or negative or zero
+
 #include <stdio.h>
 int main(){
+  double num;
+  printf("Enter a number: ");
+  scanf("%lf", &num);
 
-  const int maxInput = 100;
-  int i;
-  double number, average, sum = 0.0;
+  if(num <= 0.0){
 
-  for(i = 1; i <= maxInput; i++){
-    printf("%d Enter a number ", i);
-    scanf("%lf", &number);
-
-    // go to jump if the user enters a negative number
-    if(number < 0.0){
-      goto jump;
+    if(num == 0.0){
+      printf("You entered 0");
+    } else {
+      printf("You entered a negative number");
     }
-    sum += number;
+    
+  } else {
+    printf("You entered a positive number");
   }
-  jump:average = sum / (i - 1);
-  printf("Sum = %.2f\n", sum);
-  printf("Average = %.2f", average);
   return 0;
 }
