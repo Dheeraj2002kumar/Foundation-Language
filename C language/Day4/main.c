697224d34761ef18f5774130b3eb1c03828cb653
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-19 08:48:24 +0530

    Add files via upload

--- C language/Day4/main.c
+++ C language/Day4/main.c
@@ -1,28 +1,49 @@
 // C program to check whether the given number is the perfect number
-#include<stdio.h>
+// #include<stdio.h>
+
+// int main(){
+
+//   // declare and initialize the  variables
+//   int num, rem, sum = 0, i;
+
+//   // take an input from the user
+//   printf("Enter a number: ");
+//   scanf("%d", &num);
+
+//   // find all divisors and then 
+//   for(i = 1; i < num; i++){
+//     rem = num % i;
+//     if(rem == 0){
+//       sum = sum + i;
+//     }
+//   }
+
+//   if(sum == num){
+//     printf("%d is a perfect number", num);
+//   } else {
+//     printf("%d is not a perfect number",num);
+//   }
+
+//   return 0;
+// }
+
+
+
+//=====================================================
+
+// Program to add numbers until the user enters zero
+#include <stdio.h>
 
 int main(){
+  double number, sum = 0;
 
-  // declare and initialize the  variables
-  int num, rem, sum = 0, i;
-
-  // take an input from the user
-  printf("Enter a number: ");
-  scanf("%d", &num);
-
-  // find all divisors and then 
-  for(i = 1; i < num; i++){
-    rem = num % i;
-    if(rem == 0){
-      sum = sum + i;
-    }
+  // the body of the loop is executed at least once 
+  do {
+    printf("Enter a number: ");
+    scanf("%lf", &number);
+    sum += number;
   }
-
-  if(sum == num){
-    printf("%d is a perfect number", num);
-  } else {
-    printf("%d is not a perfect number",num);
-  }
-
+  while (number != 0.0);
+  printf("sum = %.2lf", sum);
   return 0;
 }
