55bd39f7aa462374fd08fc8e021b6ab5c468f706
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-20 08:51:10 +0530

    Add files via upload

--- C language/Day5/main.c
+++ C language/Day5/main.c
@@ -1,25 +1,53 @@
+// // Example: User-defined function
+
+// #include <stdio.h>
+
+// int addNumbers(int a, int b);   // function prototype
+
+// int main(){
+//   int n1, n2, sum;
+//   printf("Enter first number: ");
+//   scanf("%d", &n1);
+
+//   printf("Enter second number: ");
+//   scanf("%d", &n2);
+
+//   sum = addNumbers(n1, n2);   // function call
+
+//   printf("Sum = %d", sum);
+//   return 0;
+// }
+
+// int addNumbers(int a, int b){     // function definition
+//   int result;
+//   result = a + b;
+//   return result;    // return statement
+// }
+
+//=======================================================
 // Example: User-defined function
+// create subtraction function
 
 #include <stdio.h>
 
-int addNumbers(int a, int b);   // function prototype
+int subtraction(int a, int b);   // function prototype
 
 int main(){
-  int n1, n2, sum;
+  int n1, n2, sub;
   printf("Enter first number: ");
   scanf("%d", &n1);
 
   printf("Enter second number: ");
   scanf("%d", &n2);
 
-  sum = addNumbers(n1, n2);   // function call
+  sub = subtraction(n1, n2);   // function call
 
-  printf("Sum = %d", sum);
+  printf("Subtraction is = %d", sub);
   return 0;
 }
 
-int addNumbers(int a, int b){     // function definition
+int subtraction(int a, int b){     // function definition
   int result;
-  result = a + b;
+  result = a - b;
   return result;    // return statement
 }
