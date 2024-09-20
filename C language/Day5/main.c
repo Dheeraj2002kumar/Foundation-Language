e16edf5d19482f19ecd9129cde5f63a5e5df3e20
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-20 09:11:46 +0530

    Add files via upload

--- C language/Day5/main.c
+++ C language/Day5/main.c
@@ -28,26 +28,50 @@
 // Example: User-defined function
 // create subtraction function
 
+// #include <stdio.h>
+
+// int subtraction(int a, int b);   // function prototype
+
+// int main(){
+//   int n1, n2, sub;
+//   printf("Enter first number: ");
+//   scanf("%d", &n1);
+
+//   printf("Enter second number: ");
+//   scanf("%d", &n2);
+
+//   sub = subtraction(n1, n2);   // function call
+
+//   printf("Subtraction is = %d", sub);
+//   return 0;
+// }
+
+// int subtraction(int a, int b){     // function definition
+//   int result;
+//   result = a - b;
+//   return result;    // return statement
+// }
+
+//=====================================================
+// Example: User-defined function with eveOdd
+// function to check whether a number is even or odd
 #include <stdio.h>
+int eveOdd(int n); // function prototype
 
-int subtraction(int a, int b);   // function prototype
-
-int main(){
-  int n1, n2, sub;
-  printf("Enter first number: ");
-  scanf("%d", &n1);
-
-  printf("Enter second number: ");
-  scanf("%d", &n2);
-
-  sub = subtraction(n1, n2);   // function call
-
-  printf("Subtraction is = %d", sub);
+int main()
+{
+  int n;
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  printf("Number is %s", eveOdd(n) == 1 ? "even" : "odd");
   return 0;
 }
-
-int subtraction(int a, int b){     // function definition
-  int result;
-  result = a - b;
-  return result;    // return statement
+// function definition
+int eveOdd(int n)
+{ // function definition
+  if (n % 2 == 0)
+    return 1; // return statement
+  else
+    return 0;
 }
+
