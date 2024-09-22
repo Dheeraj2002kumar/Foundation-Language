171953f53953cad731af791fb019b8c355972948
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-22 10:19:32 +0530

    Add files via upload

--- C language/Day6/main.c
+++ C language/Day6/main.c
@@ -1,26 +1,53 @@
 //============================================================
 // Example: Sum of natural Number using recursion
 
+// #include<stdio.h>
+
+// int sum(int n);
+
+// int main(){
+//   int number, result; 
+//   printf("Enter a positive integer: ");
+//   scanf("%d", &number);
+
+//   result = sum(number);
+
+//   printf("Sum = %d", result);
+//   return 0;
+// }
+
+// int sum(int n){
+//   if(n != 0){
+//     // sum() function call itself
+//     return n + sum(n-1);
+//   } else {
+//     return n;
+//   }
+// }
+
+
+
+//====================================================================
+
+// C storage class: Local Variable
+// A local variable is a variable that is declared inside a function or a block of code. It
+// is used to store a value that is only accessible within that function or block of code.
+// The storage class of a local variable is auto by default.
+
+// The auto storage class is used to declare local variables. It is the default storage class
+// for local variables.
+// The auto storage class is used to declare local variables. It is the default storage class
+// for local variables.
+// The auto storage class is used to declare local variables. It is the default storage class
+// for local variables.
+
 #include<stdio.h>
-
-int sum(int n);
-
-int main(){
-  int number, result; 
-  printf("Enter a positive integer: ");
-  scanf("%d", &number);
-
-  result = sum(number);
-
-  printf("Sum = %d", result);
-  return 0;
-}
-
-int sum(int n){
-  if(n != 0){
-    // sum() function call itself
-    return n + sum(n-1);
-  } else {
-    return n;
+int main(void) {
+  for(int i = 0; i<5; i++){
+    printf("C programming");
   }
+
+  // Error: i is not declared at this point 
+  printf("%d",i);
+  return 0;
 }
