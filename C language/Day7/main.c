1100043140941854227bd0798259cd1c459667e4
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-24 13:24:30 +0530

    Add files via upload

--- C language/Day7/main.c
+++ C language/Day7/main.c
@@ -1,30 +1,63 @@
 // C program to store tempreture of two cities of a week and display it.
 
+// #include<stdio.h>
+
+// const int CITY = 2;
+// const int WEEK = 7;
+
+// int main(){
+//   int temperature[CITY][WEEK];
+//   int i, j;
+
+//   // Using nested loop to store values in a 2d array
+//   for(i = 0; i < CITY; i++){
+//     for(j = 0; j < WEEK; j++){
+//       printf("City %d, Day %d ", i+1, j+1);
+//       scanf("%d", &temperature[i][j]);
+//     }
+//     printf("\n");
+//   }
+//   printf("\nDisplaying values: \n\n");
+
+//   // Using nested toop to display values of a 2d array
+//   for(i = 0; i < CITY; i++){
+//     for(j = 0; j<WEEK; j++){
+//       printf("City %d, Day %d = %d\n", i+1, j+1, temperature[i][j]);
+//     }
+//     printf("\n");
+//   }
+//   return 0;
+// }
+
+
+//==========================================================
+// C Program to store and print 12 values entered by the user
+
 #include<stdio.h>
-
-const int CITY = 2;
-const int WEEK = 7;
-
 int main(){
-  int temperature[CITY][WEEK];
-  int i, j;
+  int test[2][3][2];
+  int i, j, k;
 
-  // Using nested loop to store values in a 2d array
-  for(i = 0; i < CITY; i++){
-    for(j = 0; j < WEEK; j++){
-      printf("City %d, Day %d ", i+1, j+1);
-      scanf("%d", &temperature[i][j]);
-    }
-    printf("\n");
-  }
-  printf("\nDisplaying values: \n\n");
+  printf("Enter 12 values: \n");
 
-  // Using nested toop to display values of a 2d array
-  for(i = 0; i < CITY; i++){
-    for(j = 0; j<WEEK; j++){
-      printf("City %d, Day %d = %d\n", i+1, j+1, temperature[i][j]);
+  for(i = 0; i < 2; i++){
+    for(j = 0; j < 3; j++){
+      for(k = 0; k < 2; k++){
+        scanf("%d", &test[i][j][k]);
+      }
     }
-    printf("\n");
   }
+
+  // printing values with proper index.
+  printf("\nDisplaying values:\n");
+
+  for(i = 0; i < 2; i++){
+    for(j = 0; j < 3; j++){
+      for(k = 0; k < 2; k++){
+        printf("test[%d][%d][%d] = %d\n", i, j, k, test[i][j][k]);
+      }
+    }
+  }
+
   return 0;
 }
