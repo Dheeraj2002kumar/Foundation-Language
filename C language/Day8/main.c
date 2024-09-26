28b9ee819d34d4c863216516974a21701d46eeb9
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-26 08:54:13 +0530

    Add files via upload

--- C language/Day8/main.c
+++ C language/Day8/main.c
@@ -53,54 +53,72 @@
 
 //======================================================
 // Store information  of n students using structures
-#include <stdio.h>
-#include<string.h>
+// #include <stdio.h>
+// #include<string.h>
 
-// Define a structure for a student
-struct Student {
-    char name[50];
-    int rollNo;
-    float marks;
-};
+// // Define a structure for a student
+// struct Student {
+//     char name[50];
+//     int rollNo;
+//     float marks;
+// };
 
-int main() {
-    // Array of structures to store information of 10 students
-    struct Student students[10];
+// int main() {
+//     // Array of structures to store information of 10 students
+//     struct Student students[10];
 
-    // Input data for 10 students
-    for (int i = 0; i < 10; i++) {
-        printf("Enter details for student %d:\n", i + 1);
+//     // Input data for 10 students
+//     for (int i = 0; i < 10; i++) {
+//         printf("Enter details for student %d:\n", i + 1);
 
-        // Using fgets to take input for the name
-        printf("Enter name: ");
-        fgets(students[i].name, sizeof(students[i].name), stdin);
+//         // Using fgets to take input for the name
+//         printf("Enter name: ");
+//         fgets(students[i].name, sizeof(students[i].name), stdin);
 
-        // Remove the newline character if present after fgets()
-        if (students[i].name[strlen(students[i].name) - 1] == '\n') {
-            students[i].name[strlen(students[i].name) - 1] = '\0';
-        }
+//         // Remove the newline character if present after fgets()
+//         if (students[i].name[strlen(students[i].name) - 1] == '\n') {
+//             students[i].name[strlen(students[i].name) - 1] = '\0';
+//         }
 
-        // Input roll number and marks
-        printf("Enter roll number: ");
-        scanf("%d", &students[i].rollNo);
+//         // Input roll number and marks
+//         printf("Enter roll number: ");
+//         scanf("%d", &students[i].rollNo);
 
-        printf("Enter marks: ");
-        scanf("%f", &students[i].marks);
+//         printf("Enter marks: ");
+//         scanf("%f", &students[i].marks);
 
-        // Clear the input buffer (to discard the newline character left by scanf)
-        getchar();
+//         // Clear the input buffer (to discard the newline character left by scanf)
+//         getchar();
         
-        printf("\n");
-    }
+//         printf("\n");
+//     }
 
-    // Display the information of the 10 students
-    printf("Displaying information of students:\n");
-    for (int i = 0; i < 10; i++) {
-        printf("Student %d:\n", i + 1);
-        printf("Name: %s\n", students[i].name);
-        printf("Roll No: %d\n", students[i].rollNo);
-        printf("Marks: %.2f\n\n", students[i].marks);
-    }
+//     // Display the information of the 10 students
+//     printf("Displaying information of students:\n");
+//     for (int i = 0; i < 10; i++) {
+//         printf("Student %d:\n", i + 1);
+//         printf("Name: %s\n", students[i].name);
+//         printf("Roll No: %d\n", students[i].rollNo);
+//         printf("Marks: %.2f\n\n", students[i].marks);
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+
+
+//============================================
+// C Pointers
+// address in c
+
+// #include<stdio.h>
+// int main(){
+//     int var = 5; 
+//     printf("var: %d\n", var);
+
+//     printf("address of var: %p", &var);
+//     return 0;
+// }
+
+
+//===============================================
