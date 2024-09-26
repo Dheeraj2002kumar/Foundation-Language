3d0c6321a8a4a8f6597d0381430d673db99f9386
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-26 09:20:40 +0530

    Add files via upload

--- C language/Day8/main.c
+++ C language/Day8/main.c
@@ -124,14 +124,42 @@
 //===============================================
 // changing value pointed by pointers
 
+// #include<stdio.h>
+// int main(){
+//     int* pc, c;
+//     c = 5;
+//     pc = &c;
+//     c = 1;
+
+//     printf("%d\n", c); //1
+//     printf("%d", *pc); //1  
+//     // * hold value of the pointer
+//     // & hold address of the pointer
+//     return 0;
+// }
+
+
+//=================================================
+// working of pointers
 #include<stdio.h>
 int main(){
     int* pc, c;
-    c = 5;
-    pc = &c;
-    c = 1;
+    c = 22;
 
-    printf("%d\n", c); //1
-    printf("%d", *pc); //1
+    printf("Address of c: %p\n", &c);
+    printf("Value of c: %d\n\n", c); // 22
+
+    pc = &c;
+
+    printf("Address of pointer pc: %pn", pc);
+    printf("Content of pointer pc: %d\n\n", *pc); // 22
+
+    c = 11;
+    printf("Address of pointer pc: %p\n", pc);
+    printf("Content of pointer pc: %d\n\n", *pc); // 11
+
+    *pc = 2;
+    printf("Address of c: %p\n", &c);
+    printf("Value of c: %d\n\n", c); // 2
     return 0;
 }
