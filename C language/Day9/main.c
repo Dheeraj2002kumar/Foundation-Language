c956b1f42cabf80f02b16b96e5eb19898df92948
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-27 09:08:25 +0530

    Add files via upload

--- C language/Day9/main.c
+++ C language/Day9/main.c
@@ -1,13 +1,42 @@
-#include<stdio.h>
+// #include<stdio.h>
 
-void addOne(int* ptr){
-  (*ptr)++; // adding 1 to ptr
-}
+// void addOne(int* ptr){
+//   (*ptr)++; // adding 1 to ptr
+// }
 
-int main(){
-  int* p, i = 10;
-  p = &i;
-  addOne(p);
-  printf("%d", *p);  // 11
-  return 0;
-}
+// int main(){
+//   int* p, i = 10;
+//   p = &i;
+//   addOne(p);
+//   printf("%d", *p);  // 11
+//   printf("\n%d", p);  // address value of c
+//   return 0;
+// }
+
+
+// //==============================================
+// // Write a program in C to add two numbers using pointers.
+// #include<stdio.h>
+// int main(){
+//   int fno, sno, *ptr, *qtr, sum;
+
+//   printf("\n\nPointer: Add two numbers: \n");
+//   printf("-------------------------------\n");
+
+//   printf("Input the first number: ");
+//   scanf("%d", &fno);
+//   printf("Input the second number: ");
+//   scanf("%d", &sno);
+
+//   ptr = &fno;
+//   qtr = &sno;
+
+//   sum = *ptr + *qtr;
+
+//   printf("The sum of the  entered numbers is: %d\n\n", sum);
+
+//   return 0;
+// }
+
+
+//===============================================
