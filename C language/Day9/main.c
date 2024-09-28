cfbff04f3deee073f1386365728c84a71daf75a0
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-28 10:04:41 +0530

    Add files via upload

--- C language/Day9/main.c
+++ C language/Day9/main.c
@@ -457,27 +457,55 @@
 
 
 //=====================================================
-// Example: malloc() and free()
+// Example 1: malloc() and free()
 // Program to calulate the sum of n numbers entered by the user
+// #include<stdio.h>
+// #include<stdlib.h>
+
+// int main(){
+//   int num, i, *ptr, sum = 0;
+//   printf("Enter the total number of elements: ");
+//   scanf("%d", &num);
+//   ptr = (int *)malloc(num * sizeof(int));
+//   if(ptr == NULL){
+//     printf("Errr ! memory not allocated.");
+//     exit(0);
+//   }
+
+//   printf("Enter elements: ");
+//   for(i = 0; i < num; i++){
+//     scanf("%d", ptr + i);
+//     sum += *(ptr + i);
+//   }
+//   printf("Sum = %d\n", sum);
+//   free(ptr);
+//   return 0;
+// }
+
+
+// ===========================================================
+// Example 2: calloc() and free()
+// program to calclulate the sum of n numbers entered by the user 
 #include<stdio.h>
 #include<stdlib.h>
-
 int main(){
-  int num, i, *ptr, sum = 0;
-  printf("Enter the total number of elements: ");
-  scanf("%d", &num);
-  ptr = (int *)malloc(num * sizeof(int));
+  int n, i, *ptr, sum = 0;
+  printf("Enter number of elements: ");
+  scanf("%d", &n);
+  ptr = (int*) calloc(n, sizeof(int));
+
   if(ptr == NULL){
-    printf("Errr ! memory not allocated.");
+    printf("Error! Memory not allocated.");
     exit(0);
   }
 
   printf("Enter elements: ");
-  for(i = 0; i < num; i++){
+  for(i = 0; i < n; i++){
     scanf("%d", ptr + i);
     sum += *(ptr + i);
   }
-  printf("Sum = %d\n", sum);
+
+  printf("Sum = %d", sum);
   free(ptr);
   return 0;
 }
