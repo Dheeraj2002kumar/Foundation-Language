d0eb6ecc693c1c4c60f907b588e1c122af443edb
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-30 10:12:04 +0530

    Add files via upload

--- C language/Day10/main.c
+++ C language/Day10/main.c
@@ -218,3 +218,18 @@
 //   printf("Number of workers = %d", j.workerNo);
 //   return 0;
 // }
+
+//=======================================================
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int *p;
+  p =  (int*)calloc(3*sizeof(int));
+  printf("Enter  first number\n");
+  scanf("%d", p);
+  printf("Enter second number\n");
+  scanf("%d", p+2);
+  printf("%d %d", *p, *(p+2));
+  free(p);
+  return 0;
+}
