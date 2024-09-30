b7e146290ccc8c59c2a4f553d2246b9cff8fdab5
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-30 18:43:23 +0530

    Add files via upload

--- C++ language/Day1/main.cpp
+++ C++ language/Day1/main.cpp
@@ -84,12 +84,34 @@
 
 //======================================================
 // C++ Program to find size of int, float, double and char in Your system
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   cout << "Size of int: " << sizeof(int) << endl;
+//   cout << "Size of float: " << sizeof(float) << endl;
+//   cout << "Size of double: " << sizeof(double) << endl;
+//   cout << "Size of char: " << sizeof(char) << endl;
+//   return 0;
+// }
+
+
+//============================================================
+// C++ Program to Swap two Numbers
 #include<iostream>
 using namespace std;
 int main(){
-  cout << "Size of int: " << sizeof(int) << endl;
-  cout << "Size of float: " << sizeof(float) << endl;
-  cout << "Size of double: " << sizeof(double) << endl;
-  cout << "Size of char: " << sizeof(char) << endl;
+  int num1, num2, temp;
+  cout << "Enter num1: " << endl;
+  cin >> num1;
+  cout << "Enter num2: " << endl;
+  cin >> num2;
+
+  temp = num1;
+  num1 = num2;
+  num2 = temp;
+
+  cout << "Value of num1 is: " << num1 << endl; 
+  cout << "Value of num2 is: " << num2 << endl; 
+  
   return 0;
 }
