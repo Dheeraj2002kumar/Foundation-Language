c63d6444603c8822d588c72c3230ee95ed1672b2
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-30 18:26:42 +0530

    Add files via upload

--- C++ language/Day1/main.cpp
+++ C++ language/Day1/main.cpp
@@ -32,18 +32,35 @@
 //=================================================
 // C++ Program to Print Number Entered by User
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     // Write C++ code here
+//     int num;
+//     cout << "Enter the number: "<< endl;
+//     cin >> num;
+//     cout << "You entered the number is "<< num << endl;
+
+//     return 0;
+// }
+
+// =========================================
+// C++ Program to Add Two Numbers
 #include <iostream>
 using namespace std;
 
 int main() {
     // Write C++ code here
-    int num;
-    cout << "Enter the number: "<< endl;
-    cin >> num;
-    cout << "You entered the number is "<< num << endl;
+    int num1, num2, sum;
+    cout << "Enter num1: " << endl;
+    cin >> num1;
+    
+    cout << "Enter num2: " << endl;
+    cin >> num2;
+    
+    sum = num1 + num2;
+    cout << "The sum of " << num1 << " + " << num2 << " = " << sum << endl;
 
     return 0;
 }
-
-// =========================================
-// C++ Program to Add Two Numbers
