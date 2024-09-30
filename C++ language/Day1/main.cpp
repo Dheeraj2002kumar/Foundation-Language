4a28379b49e3e2e6cd6161c1742b8e3bda5cfb81
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-30 18:34:00 +0530

    Add files via upload

--- C++ language/Day1/main.cpp
+++ C++ language/Day1/main.cpp
@@ -47,20 +47,37 @@
 
 // =========================================
 // C++ Program to Add Two Numbers
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     // Write C++ code here
+//     int num1, num2, sum;
+//     cout << "Enter num1: " << endl;
+//     cin >> num1;
+    
+//     cout << "Enter num2: " << endl;
+//     cin >> num2;
+    
+//     sum = num1 + num2;
+//     cout << "The sum of " << num1 << " + " << num2 << " = " << sum << endl;
+
+//     return 0;
+// }
+
+//========================================================
+// C++ Program to find Quotient and Remainder
+#include<iostream>
 using namespace std;
-
-int main() {
-    // Write C++ code here
-    int num1, num2, sum;
-    cout << "Enter num1: " << endl;
-    cin >> num1;
-    
-    cout << "Enter num2: " << endl;
-    cin >> num2;
-    
-    sum = num1 + num2;
-    cout << "The sum of " << num1 << " + " << num2 << " = " << sum << endl;
-
-    return 0;
+int main(){
+  int num1, num2, quotient, remainder;
+  cout << "Enter num1: " << endl;
+  cin >> num1;
+  cout << "Enter num2: " << endl;
+  cin >> num2;
+  quotient = num1 / num2;
+  remainder = num1 % num2;
+  cout << "Quotient: " << quotient << endl;
+  cout << "Remainder: " << remainder << endl;
+  return 0;
 }
