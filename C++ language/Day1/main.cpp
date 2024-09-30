ffc21dfcece581401912fdc3dbc0f0a8a9c9de2d
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-09-30 18:37:35 +0530

    Add files via upload

--- C++ language/Day1/main.cpp
+++ C++ language/Day1/main.cpp
@@ -67,17 +67,29 @@
 
 //========================================================
 // C++ Program to find Quotient and Remainder
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int num1, num2, quotient, remainder;
+//   cout << "Enter num1: " << endl;
+//   cin >> num1;
+//   cout << "Enter num2: " << endl;
+//   cin >> num2;
+//   quotient = num1 / num2;
+//   remainder = num1 % num2;
+//   cout << "Quotient: " << quotient << endl;
+//   cout << "Remainder: " << remainder << endl;
+//   return 0;
+// }
+
+//======================================================
+// C++ Program to find size of int, float, double and char in Your system
 #include<iostream>
 using namespace std;
 int main(){
-  int num1, num2, quotient, remainder;
-  cout << "Enter num1: " << endl;
-  cin >> num1;
-  cout << "Enter num2: " << endl;
-  cin >> num2;
-  quotient = num1 / num2;
-  remainder = num1 % num2;
-  cout << "Quotient: " << quotient << endl;
-  cout << "Remainder: " << remainder << endl;
+  cout << "Size of int: " << sizeof(int) << endl;
+  cout << "Size of float: " << sizeof(float) << endl;
+  cout << "Size of double: " << sizeof(double) << endl;
+  cout << "Size of char: " << sizeof(char) << endl;
   return 0;
 }
