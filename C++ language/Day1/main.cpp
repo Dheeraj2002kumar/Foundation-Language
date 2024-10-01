3c5b086df71abf3aa61ef7b4e8f0261a1bac2d7e
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-01 09:13:38 +0530

    Add files via upload

--- C++ language/Day1/main.cpp
+++ C++ language/Day1/main.cpp
@@ -118,13 +118,76 @@
 
 //=========================================================
 // C++ Program to Find ASCII Value of a Caracter
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   char ch;
+//   cout << "Enter a character: " << endl;
+//   cin >> ch;
+//   cout << "ASCII value of " << ch << " is: " << int(ch) << endl;
+
+//   return 0;
+// }
+
+//====================================================
+// C++ Program to Multiply  two number
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   int num1, num2, Multiply;
+//   cout << "Enter num1: " << endl;
+//   cin >> num1;
+//   cout << "Enter num2: " << endl;
+//   cin >> num2;
+
+//   Multiply = num1 * num2;
+
+//   cout << "Multiplication of " << num1 << " * " << num2 << " = " << Multiply << endl;
+//   return 0;
+// }
+
+
+//=======================================================
+// C++ Program to Find Factorial of a Number
+// #include <iostream> 
+// using namespace std;
+// int main(){
+//   int num, i, factorial = 1;
+//   cout << "Enter a number: " << endl;
+//   cin >> num;
+//   for (i = 1; i <= num; i++)
+//   {
+//     factorial = factorial * i;
+//   }
+//   cout << "Factorial of " << num << " = " << factorial << endl;
+//   return 0;
+// }
+
+//===========================================================
+// C++ program to find if an integer is even or odd or neither (0) using nested if statements.
 #include<iostream>
 using namespace std;
-int main(){
-  char ch;
-  cout << "Enter a character: " << endl;
-  cin >> ch;
-  cout << "ASCII value of " << ch << " is: " << int(ch) << endl;
 
+int main(){
+  int num;
+  cout << "Enter an integer: ";
+  cin >> num;
+
+  // outer if condition
+  if(num != 0){
+    // inner if condition
+    if((num % 2) == 0){
+      cout << "The number is even." << endl;
+    }
+    // inner else condition
+    else{
+      cout << "The number is odd." << endl;
+    }
+  }
+  // outer else condition
+  else {
+    cout << "The number is 0 and it is neither even nor odd." << endl;
+  }
+  cout << "This line is always printed." << endl;
   return 0;
 }
