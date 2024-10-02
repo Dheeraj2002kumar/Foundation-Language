036c289aeb67a1152254443ef76da775721c8756
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-02 08:02:46 +0530

    Add files via upload

---
+++ C++ language/Day2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	if(0){
+		std::cout <<"Good bye";
+	} else {
+		std::cout << "Good";
+	}
+	return 0;
+}
