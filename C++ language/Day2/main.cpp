89aa8966733c4e359b0f847c3d9f138dfad33180
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-02 08:37:02 +0530

    Add files via upload

--- C++ language/Day2/main.cpp
+++ C++ language/Day2/main.cpp
@@ -11,32 +11,53 @@
 //	return 0;
 //}
 
-//===============================================
-// This program calcuates the average of numbers entered by the user.
-// If the user enters a negative number, it ignores the number and
-// calculates the average number entered before it.
+////===============================================
+//// This program calcuates the average of numbers entered by the user.
+//// If the user enters a negative number, it ignores the number and
+//// calculates the average number entered before it.
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main(){
+//	float num, average, sum = 0.0;
+//	int i, n;
+//	cout << "Maximum number of inputs: ";
+//	cin >> n;
+//	
+//	for(i = 1; i <= n; i++){
+//		cout << "Enter n" << i << ": ";
+//		cin >> num;
+//		
+//		if(num < 0.0){
+//			// Control of the program move to jump:
+//			goto jump;
+//		}
+//		sum += num;
+//	}
+//	jump:
+//		average = sum / (i - 1);
+//		cout << "\nAverage = " << average;
+//		return 0;
+//}
+
+
+//==============================================
+// C++ Program to check whether number is Even or Odd
 
 #include <iostream>
 using namespace std;
 
 int main(){
-	float num, average, sum = 0.0;
-	int i, n;
-	cout << "Maximum number of inputs: ";
-	cin >> n;
+	int num;
+	cout << "Enter number: ";
+	cin >> num;
 	
-	for(i = 1; i <= n; i++){
-		cout << "Enter n" << i << ": ";
-		cin >> num;
-		
-		if(num < 0.0){
-			// Control of the program move to jump:
-			goto jump;
-		}
-		sum += num;
+	if(num % 2 == 0){
+		cout << "Number is Even...";
 	}
-	jump:
-		average = sum / (i - 1);
-		cout << "\nAverage = " << average;
-		return 0;
+	else {
+		cout << "Number is Odd...";
+	}
+	return 0;
 }
