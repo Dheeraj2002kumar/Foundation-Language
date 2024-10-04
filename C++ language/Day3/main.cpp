8ab9772818dc4418ef93bad0f49a642a7bced20d
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-04 23:23:46 +0530

    Add files via upload

--- C++ language/Day3/main.cpp
+++ C++ language/Day3/main.cpp
@@ -1,7 +1,24 @@
 // C++ Recursion, Arrays, Strings, Structures
+
+// Factorial of n = 1*2*3*...*n
 #include <iostream>
 using namespace std;
+
+int factorial(int); // functoin prototype
+
 int main(){
-	cout << "Hello Dheeraj...";
+	int n, result;
+	cout << "Enter a non-negative number: ";
+	cin >> n;
+	result = factorial(n);
+	cout << "Factorial of " << n << " = " << result;	
 	return 0;
+}
+
+int factorial(int n) {
+	if(n > 1){
+		return n * factorial(n-1);
+	} else {
+		return 1;
+	}
 }
