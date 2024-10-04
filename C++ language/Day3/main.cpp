2619ede131809a363442a16dc340043c1fe23c27
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-04 23:54:23 +0530

    Add files via upload

--- C++ language/Day3/main.cpp
+++ C++ language/Day3/main.cpp
@@ -1,24 +1,53 @@
 // C++ Recursion, Arrays, Strings, Structures
 
 // Factorial of n = 1*2*3*...*n
+//#include <iostream>
+//using namespace std;
+//
+//int factorial(int); // functoin prototype
+//
+//int main(){
+//	int n, result;
+//	cout << "Enter a non-negative number: ";
+//	cin >> n;
+//	result = factorial(n);
+//	cout << "Factorial of " << n << " = " << result;	
+//	return 0;
+//}
+//
+//int factorial(int n) {
+//	if(n > 1){
+//		return n * factorial(n-1);
+//	} else {
+//		return 1;
+//	}
+//}
+
+
+
+
+//======================================================
+// C++ Return by Reference
+/*
+In C++ Programming, not only can you pass values by reference
+to a function but you can also return a value by reference.
+*/
+
 #include <iostream>
 using namespace std;
 
-int factorial(int); // functoin prototype
+// Global variable
+int num;
+
+// function declaration 
+int& test();
 
 int main(){
-	int n, result;
-	cout << "Enter a non-negative number: ";
-	cin >> n;
-	result = factorial(n);
-	cout << "Factorial of " << n << " = " << result;	
+	test() = 5;
+	cout << num;
 	return 0;
 }
 
-int factorial(int n) {
-	if(n > 1){
-		return n * factorial(n-1);
-	} else {
-		return 1;
-	}
+int& test(){
+	return num;
 }
