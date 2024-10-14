273c3824c143ddc2ba9e0a555e74231e13e62283
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-14 23:01:06 +0530

    Add files via upload

--- C++ language/Day4/main.cpp
+++ C++ language/Day4/main.cpp
@@ -1,4 +1,5 @@
 // Enumeration
+/*
 #include <iostream>
 using namespace std;
 
@@ -11,3 +12,37 @@ int main(){
 	cout << "Day" << today + 1;
 	return 0;
 }
+*/
+
+/*
+Day4
+--------------------------------
+Process exited after 0.1415 seconds with return value 0
+*/
+
+//=========================================================
+// Enumeration ex: 2
+
+#include <iostream>
+using namespace std;
+
+enum seasons{
+	spring = 34, 
+	summer = 4, 
+	automn = 9, 
+	winter = 32
+};
+
+int main(){
+	seasons s;
+	s = summer;
+	cout << "Summer = " << s << endl;
+	return 0;
+}
+
+/*
+Summer = 4
+
+--------------------------------
+Process exited after 0.01113 seconds with return value 0
+*/
