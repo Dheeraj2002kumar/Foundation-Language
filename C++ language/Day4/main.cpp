e6bb9123712c9679ac5a449ec8e0e4a7708bc2c0
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-15 00:29:59 +0530

    Add files via upload

--- C++ language/Day4/main.cpp
+++ C++ language/Day4/main.cpp
@@ -54,6 +54,7 @@ Process exited after 0.01113 seconds with return value 0
 // program to illustrate the working of objects and 
 // class in c++ programming
 
+/*
 #include <iostream>
 using namespace std;
 
@@ -88,7 +89,7 @@ int main(){
 	
 	return 0;
 }
-
+*/
 
 /*
 Area of Room = 1309
@@ -97,4 +98,54 @@ Volume of Room = 25132.8
 --------------------------------
 Process exited after 0.1369 seconds with return value 0
 Press any key to continue . . .
+*/
+
+//===============================================
+// Program to illustrate the working of Public and private in C++ class
+
+#include <iostream>
+using namespace std;
+
+class Room{
+	private:
+		double length;
+		double breadth;
+		double height;
+		
+	public:
+		// function to initialize private variables
+		void getData(double len, double brth, double hgt){
+			length = len;
+			breadth = brth;
+			height = hgt;
+		}
+		
+		double calculateArea(){
+			return length * breadth;
+		}
+		
+		double calculateVolume(){
+			return length * breadth * height;
+		}
+};
+
+int main(){
+	// create object of Room class
+	Room room1;
+	// pass the values of private variables as arguments
+	room1.getData(42.5, 30.8, 19.2);
+	
+	cout << "Area of Room = " << room1.calculateArea() << endl;
+	cout << "Volume of Room = " << room1.calculateVolume() << endl;
+	
+	return 0;
+}
+
+
+/*
+Area of Room = 1309
+Volume of Room = 25132.8
+
+--------------------------------
+Process exited after 0.06076 seconds with return value 0
 */
