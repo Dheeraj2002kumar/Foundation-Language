6b54cbf17f52974e1b8e560923388f4e410be41f
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-16 07:59:32 +0530

    Add files via upload

--- C++ language/Day5/main.cpp
+++ C++ language/Day5/main.cpp
@@ -40,6 +40,8 @@ Length = 5.5
 Process exited after 0.1771 seconds with return value 0
 */
 
+
+
 //==============================================
 /*
 C++ Parameterized Constructor
@@ -49,7 +51,7 @@ constructor. This is the preferred method to initialize member data.
 C++ Program to calculate the area of a wall
 */
 
-
+/*
 #include <iostream>
 using namespace std;
 
@@ -81,3 +83,74 @@ int main(){
 	
 	return 0;
 }
+*/
+
+/*
+Area of Wall1: 90.3
+Area of Wall2: 53.55
+
+--------------------------------
+Process exited after 0.1177 seconds with return value 0
+Press any key to continue . . .
+*/
+
+
+
+//==================================================
+// C++ Copy Constructor
+// The copy constructor in C++ is used to copy data of one object to another
+
+#include <iostream>
+using namespace std;
+
+// declare a class 
+class Wall{
+	private:
+		double length;
+		double height;
+		
+	public:
+		// parameterized constructor
+		Wall(double len, double hgt){
+			// initialize private variables
+			length = len;
+			height = hgt;
+		}
+		
+		// copy constructor with a wall object as parameter
+		Wall(Wall &obj){
+			// initialize private variables
+			length = obj.length;
+			height = obj.height;
+		}
+		
+		double calculateArea(){
+			return length * height;
+		}
+};
+
+int main(){
+	// Create an object of Wall class
+	Wall wall1(10.5, 8.6);
+	
+	// print area of wall1
+	cout << "Area of Wall 1: " << wall1.calculateArea() << endl;
+	
+	// Copy contents of wall1 to another object wall2
+	Wall wall2 = wall1;
+	
+	// print area of wall2
+	cout << "Area of Wall 2: " << wall2.calculateArea() << endl;
+	
+	return 0;
+}
+
+
+/*
+Area of Wall 1: 90.3
+Area of Wall 2: 90.3
+
+--------------------------------
+Process exited after 0.1467 seconds with return value 0
+Press any key to continue . . .
+*/
