4556e927f7b39463fb3525487c2c3bb1279a4f1e
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-16 07:38:34 +0530

    Add files via upload

--- C++ language/Day5/main.cpp
+++ C++ language/Day5/main.cpp
@@ -4,6 +4,7 @@ A constructor with no parameters is known as a default constructor.
 C++ program to demonstrate the use of default constructor
 */
 
+/*
 #include <iostream>
 using namespace std;
 
@@ -29,7 +30,7 @@ int main(){
 	
 	return 0;
 }
-
+*/
 
 /*
 Create a wall.
@@ -38,3 +39,45 @@ Length = 5.5
 --------------------------------
 Process exited after 0.1771 seconds with return value 0
 */
+
+//==============================================
+/*
+C++ Parameterized Constructor
+In C++, a constructor with parameters is known as a parameterized
+constructor. This is the preferred method to initialize member data.
+
+C++ Program to calculate the area of a wall
+*/
+
+
+#include <iostream>
+using namespace std;
+
+// declare a class
+class Wall{
+	private:
+		double length;
+		double height;
+		
+	public:
+		// creae parameterized constructor
+		Wall(double len, double hgt){
+			// initialize private variables
+			length = len;
+			height = hgt;
+		}
+		double calculateArea(){
+			return length * height;
+		}
+};
+
+int main(){
+	// create object and initialize data members
+	Wall wall1(10.5, 8.6);
+	Wall wall2(8.5, 6.3);
+	
+	cout << "Area of Wall1: " << wall1.calculateArea() << endl;
+	cout << "Area of Wall2: " << wall2.calculateArea() << endl;
+	
+	return 0;
+}
