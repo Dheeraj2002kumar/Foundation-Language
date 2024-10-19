79c0af17c4283550bbb61112aedc101cb8b9d63b
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-19 11:02:19 +0530

    Add files via upload

--- C++ language/Day8/main.cpp
+++ C++ language/Day8/main.cpp
@@ -238,8 +238,9 @@ Press any key to continue . . .
 
 
 //==========================================
-// file handling
+// file handling --> create new file
 
+/*
 #include<fstream>
 #include<iostream>
 using namespace std;
@@ -257,12 +258,43 @@ int main(){
 	
 	return 0;
 }
-
+*/
 
 /*
 New file created
 --------------------------------
 Process exited after 0.1531 seconds with return value 0
+Press any key to continue . . .
+*/
+
+
+
+//===============================================
+// file handling --> writing too a file
+
+
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+	fstream st; // Step 1: create object of fstream class
+	st.open("C:/Users/hp/Desktop/TCS IT/C++ language/Day8/program.txt", ios::out); // Step 2: creating new file
+	if(!st) // step 3: checking whether file exist
+	{
+		cout << "File creation failed";
+	} else {
+		cout << "New file created";
+		st << "Hello"; // step 4: writing to file
+		st.close(); // step 5: closing file
+	}
+	return 0;
+}
+
+/*
+New file created
+--------------------------------
+Process exited after 0.01468 seconds with return value 0
 Press any key to continue . . .
 */
 
