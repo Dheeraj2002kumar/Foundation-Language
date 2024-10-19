8d3f3ea2d993b1ad7c564acb9fb547e3356a5a76
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-19 09:37:21 +0530

    Add files via upload

--- C++ language/Day8/main.cpp
+++ C++ language/Day8/main.cpp
@@ -1,5 +1,6 @@
 // C++ program to demonstrate the working of friend function
 
+/*
 #include<iostream>
 using namespace std;
 
@@ -26,12 +27,66 @@ int main(){
 	cout << "Distance: " << addFive(D);
 	return 0;
 }
-
+*/
 
 
 /*
 Distance: 5
 --------------------------------
 Process exited after 0.1592 seconds with return value 0
+Press any key to continue . . .
+*/
+
+
+
+
+//===============================================
+// Add members of two different classes using friend functions
+
+
+#include<iostream>
+using namespace std;
+
+// forward declaration
+class ClassB;
+class ClassA{
+	public:
+		// constructor to initialize numA to 12
+		ClassA(): numA(12){}
+	
+	private:
+		int numA;
+		// friend funciton declaration
+		friend int add(ClassA, ClassB);
+};
+
+class ClassB{
+	public:
+		// constructor to initialize  numB to 1
+		ClassB(): numB(1){}
+		
+	private:
+		int numB;
+		// friend funciton declaration
+		friend int add(ClassA, ClassB);
+};
+
+// access members of both classes
+int add(ClassA objectA, ClassB objectB){
+	return (objectA.numA + objectB.numB);
+}
+
+int main(){
+	ClassA objectA;
+	ClassB objectB;
+	cout << "Sum: " << add(objectA, objectB);
+	return 0;
+}
+
+
+/*
+Sum: 13
+--------------------------------
+Process exited after 0.1271 seconds with return value 0
 Press any key to continue . . .
 */
