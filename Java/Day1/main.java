b8335e1f4b49682051af4e42903f8c2b57b05336
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-21 13:13:21 +0530

    Add files via upload

---
+++ Java/Day1/main.java
@@ -0,0 +1,15 @@
+public class main {
+  public static void main(String[] args){
+
+    double myDouble = 3.4;
+    float myFloat = 3.4F;
+
+    // 3.445 * 10^2
+    double myDoubleScientific = 3.44e2;
+
+    System.out.println(myDouble); // prints 3.4
+    System.out.println(myFloat); // prints 3.4
+    System.out.println(myDoubleScientific); // 344.0
+  }
+  
+}
