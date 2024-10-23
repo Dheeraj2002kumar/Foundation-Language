26b8b89d3b09a212c808a95f1ad5264b0f874813
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-23 09:24:48 +0530

    Add files via upload

--- Java/Day2/control_flow.java
+++ Java/Day2/control_flow.java
@@ -39,6 +39,8 @@ public class control_flow {
         System.err.println("Statement outside if...else block");
        */
 
+
+       /*
        // if-else ladder
        int number = 10;
        // checks if number is greater than 0
@@ -53,5 +55,33 @@ public class control_flow {
        else{
         System.err.println("This number is 0.");
        }
+         */
+
+      // Nested if..else statement
+      Double n1 = -1.0, n2 = 4.5, n3 = -5.3, largest;
+
+      // checks if n1 is greater than or equal to n2
+      if(n1 >= n2){
+        // if..else statement inside the block
+        // check if n1 is greater than or equal to n3
+        if(n1 >= n3){
+          largest = n1;
+        }
+        else{
+          largest = n3;
+        }
+
+      }
+      else{
+        // if..else statement inside else block
+        // check if n2 is greater than or equal to n3
+        if(n2 >= n3){
+          largest = n2;
+        }
+        else{
+          largest = n3;
+        }
+      }
+      System.err.println("The largest number is " + largest);
   }
 }
