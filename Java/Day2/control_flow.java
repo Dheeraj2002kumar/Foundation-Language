64a95f1eb7716b1559be661ad33fb7906ce6d090
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-23 08:54:22 +0530

    Add files via upload

---
+++ Java/Day2/control_flow.java
@@ -0,0 +1,15 @@
+public class control_flow {
+  public static void main(String[] args) {
+    System.err.println("================ if statement =====================");
+    System.err.println();
+
+    // if statement
+    int number = 10;
+    
+    // check if number is greater than 0
+    if(number > 0){
+      System.err.println("The number is positive.");
+    }
+    System.err.println("Statement outside if block");
+  }
+}
