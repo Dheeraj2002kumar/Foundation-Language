23774ac533528f231470b647999cf3484ca8ef9d
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-23 09:07:45 +0530

    Add files via upload

--- Java/Day2/control_flow.java
+++ Java/Day2/control_flow.java
@@ -1,8 +1,9 @@
 public class control_flow {
   public static void main(String[] args) {
-    System.err.println("================ if statement =====================");
+    System.err.println("================ Control Flow =====================");
     System.err.println();
 
+    /*
     // if statement
     int number = 10;
     
@@ -11,5 +12,29 @@ public class control_flow {
       System.err.println("The number is positive.");
     }
     System.err.println("Statement outside if block");
+
+    // create a string variable
+    String languageString = "Java";
+
+    // if statement
+    if(languageString == "Java"){
+      System.err.println("Best Programming Language...");
+    }
+
+       */
+      // if-else
+      int number = 10;
+      // check if number is greater than 0
+      if(number > 0){
+        System.err.println("The number is positive.");
+      }
+        // execute this block
+        // if number is not greater than 0
+        else{
+          System.err.println("The number is not positive.");
+        }
+
+        System.err.println("Statement outside if...else block");
+      
   }
 }
