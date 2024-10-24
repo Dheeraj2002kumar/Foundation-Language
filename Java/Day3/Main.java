de2fd001820a8a915cc9e765015d586547dc0dc1
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-24 11:30:51 +0530

    Add files via upload

---
+++ Java/Day3/Main.java
@@ -0,0 +1,11 @@
+package Day3;
+
+public class Main {
+  public static void main(String[] args){
+    // program to print numbers from 1 to 5
+    int n = 5;
+    for (int i = 1; i <= n; i++){
+      System.out.println(i);
+    }
+  }
+}
