42e5812b184cba213960c0599169ca6f3cfeaa26
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-24 11:44:20 +0530

    Add files via upload

--- Java/Day3/Main.java
+++ Java/Day3/Main.java
@@ -2,10 +2,23 @@ package Day3;
 
 public class Main {
   public static void main(String[] args){
+
+    /*
     // program to print numbers from 1 to 5
     int n = 5;
     for (int i = 1; i <= n; i++){
       System.out.println(i);
+    */
+
+    // Program to find the sum of natural numbers from 1 to 1000.
+    int sum = 0;
+    int n = 1000;
+
+    // for loop
+    for(int i = 1; i <= n; i++){
+      // body inside for loop
+      sum += i; // sum = sum + i
     }
+    System.out.println("Sum of natural numbers from 1 to 1000 is: " + sum); // 500500
   }
 }
