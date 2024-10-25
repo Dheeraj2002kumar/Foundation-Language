b84ebb3f14d9055cfee936c2edf0a0dfde5eca87
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-25 10:02:00 +0530

    Add files via upload

--- Java/Day3/Main.java
+++ Java/Day3/Main.java
@@ -140,6 +140,9 @@ public class Main {
 
 
     //-------------------------------------------------------------
+
+/*
+    // labeled break statement
     // the  fr loop is labeled as first
     first:
     for(int i = 1; i < 5; i++){
@@ -154,6 +157,30 @@ public class Main {
         }
       }
     }
-  }
+       */
+
+
+
+
+  // -------------------------------------------------------
+
+
+  // labeled continue statement
+   // the  fr loop is labeled as first
+   first:
+   for(int i = 1; i < 5; i++){
+     // the for loop is labeled as second
+     second:
+     for(int j = 1; j < 3; j++){
+       System.out.println("i = " + i + "; j = " + j);
+
+       // the break statement breaks the first for loop
+       if(i == 2){
+         continue first;
+       }
+     }
+   }
+
+}
 
 }
