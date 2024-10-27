67ceb978b91230f17cc7ad4665d9f8e0c0ef401b
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-27 11:03:09 +0530

    Add files via upload

--- Java/Day4/Main.java
+++ Java/Day4/Main.java
@@ -1,6 +1,7 @@
 package Day4;
 
 import java.util.Arrays;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
@@ -108,7 +109,7 @@ public class Main {
 
  //--------------------------------------------------------
 
- /* */
+ /* 
  // java copy Arrays
 
  int[] numbers = {1,2,3,4,5,6};
@@ -126,7 +127,7 @@ System.out.println();
  // elements to be copied. The range is specified by the start and end indices.
 
  
- /* */
+ */
 
 
  
@@ -148,5 +149,30 @@ System.out.println();
  System.out.println(Arrays.toString(destination)); // [1, 2, 3, 4, 5, 6]
  */
 
+
+
+
+ //---------------------------------------------------------------------
+
+ // To use Arrays.toString() method
+
+ int[] n1 = {2, 3, 12, 4, 12, -2};
+
+ int[] n3 = new int[5];
+
+ // Creating n2 array of having length of n1 array
+ int[] n2 = new int[n1.length];
+
+ // copying entire n1 array to n2
+ System.arraycopy(n1, 0, n2, 0, n1.length);
+ // copying entire n1 array to n3
+ System.out.println("n2 = " + Arrays.toString(n2));
+
+ // copying elements from index 2 on n1 array
+ // copying element to index 1 of n3 array
+ // 2 elements wil be copied
+ System.arraycopy(n1, 2, n3, 1, 2);
+ // printing n3 array
+ System.out.println("n3 = "  + Arrays.toString(n3));
   }
 }
