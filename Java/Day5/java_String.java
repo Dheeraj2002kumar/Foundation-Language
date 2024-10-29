f5ef33b39ff10a76e8941f1f3a807f8d3634eba9
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-29 09:33:11 +0530

    Add files via upload

---
+++ Java/Day5/java_String.java
@@ -0,0 +1,24 @@
+package Day5;
+// java String
+public class java_String {
+  public static void main(String[] args) {
+    // create strings
+    String first = "java";
+    String second = "Python";
+    String third = "JavaScript";
+
+    // print strings
+    System.out.println(first); // print java
+    System.out.println(second); // print Python
+    System.out.println(third); // print JavaScript
+  }
+}
+
+
+// ============= output ========================
+// PS C:\Users\hp\Desktop\TCS IT\Java> javac .\Day5\java_String.java
+// PS C:\Users\hp\Desktop\TCS IT\Java> java .\Day5\java_String.java 
+// java
+// Python
+// JavaScript
+// PS C:\Users\hp\Desktop\TCS IT\Java> 
