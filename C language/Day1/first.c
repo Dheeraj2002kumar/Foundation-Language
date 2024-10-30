9a70eccc69af582a2e21f7e505901cfa35550f0b
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-30 14:49:44 +0530

    Add files via upload

---
+++ C language/Day1/first.c
@@ -0,0 +1,16 @@
+// #include<stdio.h>
+// int main(){
+//   printf("\n C Program");
+//   return 0;
+// }
+
+// Input function 
+#include<stdio.h>
+#include<conio.h>
+int main(){
+  int marks;
+  printf("\n Enter marks: ");
+  scanf("%d",&marks);
+  printf("marks: %d", marks);
+  return 0;
+}
---
+++ C language/Day1/main.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+  
+  printf("Dheeraj Kumar");
+  return 0;
+}
