b4184247d18312ab68ffa77f7d68dbe94440f90b
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-10-30 15:13:37 +0530

    Add files via upload

---
+++ Java/Day1/HelloWorld.java
@@ -0,0 +1,9 @@
+// Java Hello World Program
+
+// Your first program
+
+class HelloWorld {
+  public static void main(String[] args){
+    System.out.println("Hello, World!");
+  }
+}
---
+++ Java/Day1/main.java
@@ -0,0 +1,52 @@
+public class main {
+  public static void main(String[] args){
+
+    /*
+    double myDouble = 3.4;
+    float myFloat = 3.4F;
+
+    // 3.445 * 10^2
+    double myDoubleScientific = 3.44e2;
+
+    System.out.println(myDouble); // prints 3.4
+    System.out.println(myFloat); // prints 3.4
+    System.out.println(myDoubleScientific); // 344.0
+   */
+
+   // boolean type
+   boolean flag = true;
+   System.out.println(flag); // print true
+
+   // byte type
+   byte myByte = 123;
+   System.out.println(myByte); // prints 123
+
+   // short type
+
+   short temperature = -200;
+   System.out.println(temperature); // -200
+
+   // int type
+   int myInt = 1234567890;
+   System.out.println(myInt); //1234567890
+
+   // long type
+   long myLong = 1234567890123L;
+   System.out.println(myLong); // 1234567890123
+
+   // double type
+   double myDouble = 3.4;
+  System.out.println(myDouble); // 3.4
+
+  // float type
+  float myFloat = 3.4F;
+  System.out.println(myFloat); // 3.4
+
+  // char type
+  char myChar = 'A';
+  char myChar1 = '\u0051';
+  System.out.println(myChar); // A
+  System.out.println(myChar1); // Q
+  }
+  
+}
