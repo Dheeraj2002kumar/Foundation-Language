8ea5b8bbaba769d4d1a9f1a7e93f1446717bcfe4
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-11-06 10:33:16 +0530

    Add files via upload

--- Java/Day8/Main.java
+++ Java/Day8/Main.java
@@ -1,3 +1,6 @@
+package Day8;
+
+/*
 // Example: Polymorphism using method overriding
 // Note: The method that is called is determined during the execution of the program. Hence, method overriding is a run-time polymorphism
 
@@ -32,3 +35,69 @@ public class Main{
 // vscode ➜ /workspaces/Foundation-Language/Java/Day8 (main) $ java Main
 // Java Programming Language
 // Common English Language
+
+ */
+
+
+
+
+ //------------------------------------------------------------
+
+
+// Polymorphis using method overloading
+
+// Note: The method that is called is determined by the compiler. Hence it is also known as compile-time polymorphism
+
+class Pattern {
+  // method without parameter
+  public void display(){
+    for(int i = 0; i < 10; i++){
+      System.out.println("*");
+    }
+  }
+
+  // method with single parameter
+  public void display(char symbol){
+    for(int i = 0; i < 10; i++){
+      System.out.println(symbol);
+    }
+  }
+}
+
+public class Main {
+
+  public static void main(String[] args){
+    // create an object of Pattern class
+    Pattern d1 = new Pattern();
+    d1.display();
+    System.out.println("\n");
+    d1.display('#');
+  }
+}
+
+
+// +++++++++++++++++++ output ++++++++++++++++++++++
+// PS C:\Users\hp\Desktop\TCS IT\Java> java Day8.Main
+// *
+// *
+// *
+// *
+// *
+// *
+// *
+// *
+// *
+// *
+
+
+// #
+// #
+// #
+// #
+// #
+// #
+// #
+// #
+// #
+// #
+
