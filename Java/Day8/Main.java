8b9cc01709bd344182145c541e32a0194bc44aca
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-11-06 05:20:39 +0000

    Encapsulation file 3

--- Java/Day8/Main.java
+++ Java/Day8/Main.java
@@ -84,3 +84,41 @@ public class Main {
 
 
  */
+
+
+
+ // ----------------------------------------------
+
+
+ // encapsulation
+
+class Area{
+  // fields to calculate area
+  int length;
+  int breadth;
+
+  // constructor to initialize values
+  Area(int length, int breadth){
+    this.length = length;
+    this.breadth = breadth;
+  }
+
+  // method to calculate area
+  public void getArea(){
+    int area = length*breadth;
+    System.out.println("Area: " + area);
+  }
+}
+
+public class Main{
+  public static void main(String[] args){
+    // create object of Area
+    // pass value of length and breadth
+    Area rectangle = new Area(5, 6);
+    rectangle.getArea();
+  }
+}
+
+// +++++++++++++++++++ output +++++++++++++++++++++++++++
+// @Dheeraj2002kumar ➜ /workspaces/Foundation-Language/Java (main) $ java Day8.Main
+// Area: 30
