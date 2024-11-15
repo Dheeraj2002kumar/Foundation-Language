19582035ef5a60e402ecb66efb296407bb7fadcd
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-11-15 20:46:35 +0000

    java list (linkedList) 2

--- Java/Day9/Java_Collection_Framework/Main.java
+++ Java/Day9/Java_Collection_Framework/Main.java
@@ -31,8 +31,9 @@ public class Main {
 // ---------------------------------------------------
 
 
-// java list
+// java list (ArrayList)
 
+/*
 import java.util.List;
 import java.util.ArrayList;
 
@@ -58,7 +59,7 @@ public class Main {
         System.out.println("Removed Element: " + removedNumber);
     }
 }
-
+ */
 
 // ++++++++++++++++++++++++++++ output ++++++++++++++++++++++++++
 
@@ -67,3 +68,55 @@ public class Main {
 // List: [4, 9, 8]
 // Accessed Element: 8
 // Removed Element: 9
+
+
+
+// -----------------------------------------------------
+
+// java list (linkedList)
+
+import java.util.List;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // Creating list using the LinkedList class
+        List<Integer> numbers = new LinkedList<>();
+
+        // add elements to the list
+        numbers.add(33);
+        numbers.add(34);
+        numbers.add(35);
+        System.out.println("List: " + numbers);
+
+        // Access element from the list
+        int number = numbers.get(2); // 2 is index position
+        System.out.println("Accessed Element: "+ number);
+
+        // Using the indexOf() method
+        int index1 = numbers.indexOf(2);
+        int index2 = numbers.indexOf(35);
+        System.out.println("Position of 2 is: " + index1);
+        System.out.println("Position of 35 is: " + index2);
+
+        // Remove element from the list
+        int removedNumber = numbers.remove(1);
+        System.out.println("Removed Element: " + removedNumber);
+
+        System.out.println("Fainl linkedList is: " + numbers);
+    }
+}
+
+
+// ++++++++++++++++++++++++ output ++++++++++++++++++++
+
+// @Dheeraj2002kumar ➜ .../Foundation-Language/Java/Day9/Java_Collection_Framework (main) $ javac Main.java 
+// @Dheeraj2002kumar ➜ .../Foundation-Language/Java/Day9/Java_Collection_Framework (main) $ java Main.java
+// List: [33, 34, 35]
+// Accessed Element: 35
+// Position of 2 is: -1
+// Position of 35 is: 2
+// Removed Element: 34
+// Fainl linkedList is: [33, 35]
