8af17dab1d10c518926271d1547f19a35e7dde06
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-11-15 20:30:40 +0000

    java list 2

--- Java/Day9/Java_Collection_Framework/Main.java
+++ Java/Day9/Java_Collection_Framework/Main.java
@@ -2,7 +2,7 @@ package Java_Collection_Framework;
 
 // ArrayList class of collections
 // The collection framework is defined in the java.util package
-
+/*
 import java.util.ArrayList;
 
 public class Main {
@@ -18,10 +18,52 @@ public class Main {
         System.out.println("ArrayList: " + animals);
     }    
 }
-
+ */
 
 // +++++++++++++++++++++++ output ++++++++++++++++++++++++
 
 // @Dheeraj2002kumar ➜ .../Foundation-Language/Java/Day9/Java_Collection_Framework (main) $ javac Main.java 
 // @Dheeraj2002kumar ➜ .../Foundation-Language/Java/Day9/Java_Collection_Framework (main) $ java Main.java
 // ArrayList: [Dog, Cat, Horse]
+
+
+
+// ---------------------------------------------------
+
+
+// java list
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // Creating list using the ArrayList class
+        List<Integer> numbers = new ArrayList<>();
+
+        // add elements to the list
+        numbers.add(4);
+        numbers.add(9);
+        numbers.add(8);
+        System.out.println("List: "+ numbers);
+
+        // Access element from the list
+        int number = numbers.get(2); // 2 is index 
+        System.out.println("Accessed Element: " + number);
+
+        // Remove element from the list
+        int removedNumber = numbers.remove(1); // 1 is index position
+        System.out.println("Removed Element: " + removedNumber);
+    }
+}
+
+
+// ++++++++++++++++++++++++++++ output ++++++++++++++++++++++++++
+
+// @Dheeraj2002kumar ➜ .../Foundation-Language/Java/Day9/Java_Collection_Framework (main) $ javac Main.java 
+// @Dheeraj2002kumar ➜ .../Foundation-Language/Java/Day9/Java_Collection_Framework (main) $ java Main.java
+// List: [4, 9, 8]
+// Accessed Element: 8
+// Removed Element: 9
